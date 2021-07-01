80167308a0070f6053f62d702132e8a8b34e8bbf
Lindsay Reynolds <78495351+ic17rey@users.noreply.github.com>
2021-07-01 10:29:37 -0400

    Read in data from csv

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,19 @@
+// Use D3 to read in the csv data
+d3.csv("./assets/data/data.csv").then(function(censusData) {
+console.log(censusData);
+});
 
+
+//log a list of names
+//     var names = tvData.map(data => data.name);
+//     console.log("names", names);
+  
+//Cast each hours value in tvData as a number using the unary + operator
+//     tvData.forEach(function(data) {
+//       data.hours = +data.hours;
+//       console.log("Name:", data.name);
+//       console.log("Hours:", data.hours);
+//     });
+//   }).catch(function(error) {
+//     console.log(error);
+//   });
