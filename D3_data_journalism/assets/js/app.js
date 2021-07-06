8e65d6955172ef85c2a3b9f54e2321edd8a82fbd
Lindsay Reynolds <78495351+ic17rey@users.noreply.github.com>
2021-07-05 22:11:18 -0400

    Setup of chart and plot healthcare vs. poverty

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,19 +1,139 @@
+// Set up the chart
+var svgWidth = 900;
+var svgHeight = 600;
+
+var margin = {
+    top: 40,
+    right: 40,
+    bottom: 100,
+    left: 100    
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create the SVG wrapper, append an SVG group to hold data, and shift the display area
+var svg = d3.select("#scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+    //.style("border", "2px solid black")
+
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
 // Use D3 to read in the csv data
 d3.csv("./assets/data/data.csv").then(function(censusData) {
-console.log(censusData);
+    console.log(censusData);
+ // Parse data, cast as numbers (For: Healthcare vs. Poverty)
+    censusData.forEach(function(censusData) {
+        censusData.healthcare = +censusData.healthcare;
+        censusData.poverty = +censusData.poverty;
+        console.log(censusData);
+    
+    });
+    // Step 2: Create scale functions
+    // ==============================
+    var xLinearScale = d3.scaleLinear()
+      .domain([8, d3.max(censusData, d => d.poverty) + 1])
+      // .domain([0, d3.max(censusData, d => d.poverty)+2])
+      .range([0, width])
+      
+    var yLinearScale = d3.scaleLinear()
+      .domain([0, d3.max(censusData, d => d.healthcare) + 2])
+      .range([height, 0]);
+
+    // Create axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Append Axes to the chart
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis)
+      .attr("stroke", "gray");
+    chartGroup.append("g")
+      .call(leftAxis)
+      .attr("stroke", "gray");
+
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(censusData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", "15")
+    .attr("fill", "lightblue")
+    .attr("opacity", ".8");
+    //.classed("stateText", true);
 });
+   
 
 
-//log a list of names
-//     var names = tvData.map(data => data.name);
-//     console.log("names", names);
-  
-//Cast each hours value in tvData as a number using the unary + operator
-//     tvData.forEach(function(data) {
-//       data.hours = +data.hours;
-//       console.log("Name:", data.name);
-//       console.log("Hours:", data.hours);
-//     });
-//   }).catch(function(error) {
-//     console.log(error);
-//   });
+
+
+//d3.select('#scatter').style('border', '2px solid black');
+
+// var width = parseFloat(d3.select('#scatter').style('width'));
+// var height = .66*width;
+
+// var svg = d3.select('#scatter').append('svg')
+//     .style('width',width)
+//     .style('height',height);
+
+// var xText = svg.append('g')
+//     .attr('r',10)
+//     .attr('transform', `translate(${width/2},${height*.98})`)
+//     .attr("text.anchor", "middle");
+
+// xText
+//     .append('text')
+//     .text('Household Income (Median)')
+//     .attr('class', 'aText inactive x')
+//     .attr('data-name', 'income')
+//     .attr('data-axis', 'x')
+
+// xText
+//     .append('text')
+//     .attr('y',-20)
+//     .text('Age (Median)')
+//     .attr('class', 'aText inactive x')
+//     .attr('data-name', 'age')
+//     .attr('data-axis', 'x')
+
+// xText
+//     .append('text')
+//     .attr('y',-40)
+//     .text('In Poverty (%)')
+//     .attr('class', 'aText active x')
+//     .attr('data-name', 'poverty')
+//     .attr('data-axis', 'x')
+
+// var yText = svg.append('g')
+//     .attr('r',10)
+//     .attr('transform', `translate(${width*.02},${height/2})rotate(-90)`)
+//     .attr("text.anchor", "middle");
+
+// yText
+//     .append('text')
+//     .text('Obese (%)')
+//     .attr('class', 'aText inactive y')
+//     .attr('data-name', 'obesity')
+//     .attr('data-axis', 'y')
+
+// yText
+//     .append('text')
+//     .attr('y',20)
+//     .text('Smokers (%)')
+//     .attr('class', 'aText inactive y')
+//     .attr('data-name', 'smokes')
+//     .attr('data-axis', 'y')
+
+// yText
+//     .append('text')
+//     .attr('y',40)
+//     .text('Lacks Healthcare (%)')
+//     .attr('class', 'aText active y')
+//     .attr('data-name', 'healthcare')
+//     .attr('data-axis', 'y')
+
